260bf02f2a668990c3079c4638c37c852a953df7
fede2570 <fede88662@gmail.com>
2025-01-22 12:53:36 -0600

    [ADD] edit completed tasks

---
+++ todoapp/src/app/pages/home/home.component.ts
@@ -0,0 +1,107 @@
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+
+import { Task } from '../../models/task.model';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  tasks = signal<Task[]>([
+    {
+      id: Date.now(),
+      title: 'Crear proyecto',
+      completed: false
+    },
+    {
+      id: Date.now(),
+      title: 'Crear componentes',
+      completed: false
+    }
+  ]);
+
+  newTaskCtrl = new FormControl('', {
+    nonNullable: true,
+    validators: [
+      Validators.required,
+    ]
+  });
+
+  changeHandler() {
+    if (this.newTaskCtrl.valid) {
+      const value = this.newTaskCtrl.value.trim();
+      if (value !== '') {
+        this.addTask(value);
+        this.newTaskCtrl.setValue('');
+      }
+    }
+  }
+
+  addTask(title: string) {
+    const newTask = {
+      id: Date.now(),
+      title,
+      completed: false
+    };
+    this.tasks.update((tasks) => [...tasks, newTask]);
+  }
+
+  deleteTasks(index: number) {
+    this.tasks.update((tasks) => tasks.filter((task, position) => position !== index));
+    console.log(this.tasks().length)
+  }
+
+  updateTask(index: number) {
+    this.tasks.update((tasks) => {
+      return tasks.map((task, position) => {
+        if (position === index) {
+          return {
+            ...task,
+            completed: !task.completed
+          }
+        }
+        return task
+      })
+    })
+  }
+
+  updateTaskEditingMode(index: number) {
+    this.tasks.update(prevState => {
+      return prevState.map((task, position) => {
+        if (position === index) {
+          return {
+            ...task,
+            editing: true
+          }
+        }
+        return {
+          //Cambiar estado de edicion a falso para las demas tareas
+          ...task,
+          editing: false
+        };
+      })
+    });
+  }
+
+  updateTaskText(index: number, event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.tasks.update(prevState => {
+      return prevState.map((task, position) => {
+        if (position === index) {
+          return {
+            ...task,
+            title: input.value,
+            editing: false
+          }
+        }
+        return task;
+      })
+    })
+  }
+
+}
---
+++ todoapp/src/app/pages/labs/labs.component.ts
@@ -0,0 +1,103 @@
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-labs',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './labs.component.html',
+  styleUrl: './labs.component.css'
+})
+export class LabsComponent {
+  welcome = 'Hola';
+  tasks = [
+    'Instalar el Angular CLI',
+    'Crear proyecto',
+    'Crear componente'
+  ];
+
+  tasks2 = signal([
+    'Instalar el Angular CLI',
+    'Crear proyecto',
+    'Crear componente'
+  ]);
+
+  name = 'David';
+  name2 = signal('David');
+  disabled = true;
+  img = 'https://w3schools.com/howto/img_avatar.png';
+
+  person = {
+    name: 'David',
+    avatar: 'https://w3schools.com/howto/img_avatar.png'
+  };
+
+  person2 = signal({
+    name: 'David',
+    avatar: 'https://w3schools.com/howto/img_avatar.png',
+    codigo: 123
+  });
+
+  colorCtrl = new FormControl();
+
+  widthCtrl = new FormControl(50, {
+    nonNullable: true
+  });
+
+  nameCtrl = new FormControl('', {
+    nonNullable: true,
+    validators: [
+      Validators.required,
+      Validators.minLength(3)
+    ]
+  });
+
+  constructor() {
+    this.colorCtrl.valueChanges.subscribe(value => {
+      console.log(value);
+    })
+  }
+
+
+  clickHandler() {
+    alert('Hola')
+  }
+
+  changeHandler(event: Event) {
+    console.log(event);
+  }
+
+  changeHandler2(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+    this.name2.set(newValue);
+  }
+
+  keydownHandler(event: KeyboardEvent) {
+    const input = event.target as HTMLInputElement;
+    console.log(input.value);
+  }
+
+  changeCode(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+    this.person2.update(prevState => {
+      return {
+        ...prevState,
+        codigo: parseInt(newValue, 10)
+      }
+    })
+  }
+
+  changeName(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+    this.person2.update(prevState => {
+      return {
+        ...prevState,
+        name: newValue
+      }
+    })
+  }
+}
